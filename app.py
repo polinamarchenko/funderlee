20d23b83889511940ecf16d5f3592e999c07e602
Polina Marchenko <polina.marchenko@yahoo.com>
2017-05-30 15:32:57 -0700

    Commit procfile, requirements, runtime

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from project import app
 
+if os.environ.get('ENV') == 'production':
+    debug = False
+else:
+    debug = True
+
 if __name__ == '__main__':
-  app.run(debug=True, port=3000)
+  app.run(debug=debug, port=3000)
